e4380a34306239141498ae10a3deaed619418a5f
gernots <gernot.starke@innoq.com>
2015-06-20 11:24:29 +0200

    created (empty) xcode project for mac-stamper

---
+++ mac-stamper/pdfstamper/pdfstamper/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  pdfstamper
+//
+//  Created by Dr. Gernot Starke on 20.06.15.
+//  Copyright (c) 2015 Dr. Gernot Starke. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
